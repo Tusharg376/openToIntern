5785bd62c6d756ba358a34cddabf309d7394b197
21tarun <tarunuday2000@gmail.com>
2023-01-09 12:37:37 +0530

    pipelines

---
+++ src/controllers/controller.js
@@ -0,0 +1,29 @@
+const collegeModel= require('../models/collegeModel')
+const internModel=require('../models/internModel')
+
+
+
+
+const createCollege = async function(req,res){
+
+}
+
+const createIntern=async function(req,res){
+
+}
+
+const collegeDetails= async function(req,res){
+    
+}
+
+
+
+
+
+
+
+
+
+module.exports.createCollege=createCollege
+module.exports.createIntern=createIntern
+module.exports.collegeDetails=collegeDetails
