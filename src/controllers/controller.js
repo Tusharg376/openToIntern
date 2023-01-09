928897e3bf5c9d25171f612cc3139664a74f29be
Tusharg376 <honeygautam0@gmail.com>
2023-01-09 12:50:00 +0530

    create college

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -5,6 +5,19 @@ const internModel=require('../models/internModel')
 
 
 const createCollege = async function(req,res){
+    try {
+	let data =  req.body
+	    let {name,fullName,logoLink} = data
+	    if(!name) return res.status(400).send({status:false,message:"Please provide name"})
+	    if(!fullName) return res.status(400).send({status:false,message:"Please provide fullName"})
+	    if(!logoLink) return res.status(400).send({status:false,message:"Please provide logoLink"})
+	    let checkName = await collegeModel.find({$and:[{name:name, isDeleted:false}]})
+	    if(checkName) return res.status(400).send({status:false,message:"Unique name is required"})
+	    let finalData = await collegeModel.create(data)
+	    res.status(201).send({status:true,data:finalData})
+} catch (error) {
+    res.status(500).send({status:false,message:error.message})
+}
 
 }
 
