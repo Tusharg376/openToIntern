1b94c14d7434b5794ae9cf27ce033875e17be7f5
21tarun <tarunuday2000@gmail.com>
2023-01-09 12:32:42 +0530

    create pipelines

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,15 @@
 const express=require('express')
 const router= express.Router()
+const controller =require('../controllers/controller')
 
 
+// router.get('/test',function(req,res){
+//     res.send("api is working")
+// })
 
-router.get('/test',function(req,res){
-    res.send("api is working")
-})
-
-
-
+router.post('/functionup/colleges', controller.createCollege)
+router.post('/functionup/interns', controller.createIntern)
+router.get('/functionup/collegeDetails', controller.collegeDetails)
 
 
 
