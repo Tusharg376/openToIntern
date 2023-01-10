d237c8e23c4130aeb833f9dfe2b48e473fa40e70
21tarun <tarunuday2000@gmail.com>
2023-01-10 12:28:53 +0530

    final commit with almost edge cases and validations

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -8,15 +8,15 @@ const ObjectId=require('mongoose').Types.ObjectId
 const createCollege = async function(req,res){
     try {
 	    let data =  req.body
-	    let {name,fullName,logoLink} = data
-	    if(!name) return res.status(400).send({status:false,message:"Please provide name"})
-        fullName=fullName.trim()
-        name=name.toUpperCase()
-	    if(!fullName) return res.status(400).send({status:false,message:"Please provide fullName"})
-        if(!logoLink) return res.status(400).send({status:false,message:"Please provide logoLink"})
-    
-        if(!logoLink.match(/^https?:\/\/+(.)+(.gif|.jpe?g|tiff?|.png|.webp|.bmp)$/))return res.status(400).send({status:false,msg:"invalid link"})    
-	    let checkName = await collegeModel.find({$and:[{name:name, isDeleted:false}]})
+	    if(!data.name) return res.status(400).send({status:false,message:"Please provide name"})
+        data.fullName=data.fullName.trim()
+        data.name=data.name.trim().toUpperCase()
+	    if(!data.fullName) return res.status(400).send({status:false,message:"Please provide fullName"})
+        if(!data.logoLink) return res.status(400).send({status:false,message:"Please provide logoLink"})
+        
+        data.logoLink=data.logoLink.trim()
+        if(!data.logoLink.match(/^https?:\/\/+(.)+(.gif|.jpe?g|tiff?|.png|.webp|.bmp)$/))return res.status(400).send({status:false,msg:"invalid link"})    
+	    let checkName = await collegeModel.find({$and:[{name:data.name, isDeleted:false}]})
 	    if(checkName.length !=0) return res.status(400).send({status:false,message:"Unique name is required"})
     
 	    let finalData = await collegeModel.create(data)
@@ -33,22 +33,26 @@ const createIntern=async function(req,res){
         if(!data) return res.status(400).send({status:false,msg:"to create Intern body data is required"})
         if(!data.name) return res.status(400).send({status:false,msg:"name is mandatory"})
 
+        data.email=data.email.trim()
         if(!data.email) return res.status(400).send({status:false,msg:"email is mandatory"})
         if(!data.email.match(/.+\@.+\..+/)) return  res.status(400).send({status:false,msg:"invalid email"})
         let ifData = await internModel.findOne({email:data.email})
         if(ifData) return res.status(400).send({status:false,msg:"email already exist"})
 
+        data.mobile=data.mobile.trim()
         if(!data.mobile) return res.status(400).send({status:false,msg:"mobile is mandatory"})
-        if(!data.mobile.match(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/)) return res.status(400).send({status:false,msg:"invailid mobile number"})
+        if(!data.mobile.match(/^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/)) return res.status(400).send({status:false,msg:"invailid mobile number"})
         ifData = await internModel.findOne({mobile:data.mobile})
         if(ifData) return res.status(400).send({status:false,msg:"mobile already exist"})
 
+        data.collegeName=data.collegeName.trim().toUpperCase()
         if(!data.collegeName) return res.status(400).send({status:false,msg:"college name is required"})
         
-        const college= await collegeModel.findOne({name:data.collegeName.toUpperCase(), isDeleted:false })
-        if(!college) return res.status(404).send({status:false,msg:"college not found"})
-        data.collegeId=college._id
         
+        const college= await collegeModel.findOne({name:data.collegeName, isDeleted:false })
+        if(!college) return res.status(404).send({status:false,msg:"college not found"})
+        
+        data.collegeId=college._id
         const saveData= await internModel.create(data)
         return res.status(201).send({status:true,data:saveData})
     }
@@ -60,9 +64,9 @@ const createIntern=async function(req,res){
 const collegeDetails= async function(req,res){
     const clgName= req.query.collegeName.toUpperCase()
     if(!clgName) return res.status(400).send({status:false,msg:"query is required to get college details"})
-    let clg= await collegeModel.findOne({name:clgName}).select({name:1,fullName:1,logoLink:1})
+    let clg= await collegeModel.findOne({name:clgName,isDeleted:false}).select({name:1,fullName:1,logoLink:1})
     if(!clg) return res.status(400).send({status:false,msg:"college not found"})
-    let data = await internModel.find({collegeId:clg._id,isDeleted:false}) 
+    let data = await internModel.find({collegeId:clg._id,isDeleted:false}).select(['_id','name','email','mobile']) 
     if(data.length==0) data=["No intern found"]
     clg = clg.toObject();
     clg._id=undefined
