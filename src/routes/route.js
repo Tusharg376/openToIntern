5665691a5e3dd687719250122ef889c67120898f
Tusharg376 <honeygautam0@gmail.com>
2023-01-14 11:08:28 +0530

    minor changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/controller')
 
 
 
+
 router.post('/functionup/colleges', controller.createCollege)
 router.post('/functionup/interns', controller.createIntern)
 router.get('/functionup/collegeDetails', controller.collegeDetails)
@@ -12,7 +13,6 @@ router.get('/functionup/collegeDetails', controller.collegeDetails)
 router.all('/*', function (req, res) {
     res.send({ status: false, message: "invalid HTTP request" })
 })
-
 
 
 
